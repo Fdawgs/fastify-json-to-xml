09bf21562d8fa1c66d89bfb74b77463b3546444a
Frazer Smith <frazer.dev@outlook.com>
2023-11-28 20:44:13 +0000

    chore: use todo jsdoc tags for inline todo comments

--- src/index.test.js
+++ src/index.test.js
@@ -68,7 +68,7 @@ describe("JSON-To-XML plugin", () => {
 			},
 		];
 
-		// TODO: use `it.concurrent.each()` once it is no longer experimental
+		/** @todo use `it.concurrent.each()` once it is no longer experimental */
 		it.each(jsonTests)("Returns $testName", async ({ headers }) => {
 			const response = await server.inject({
 				method: "GET",
