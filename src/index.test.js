273ea6eeb3a4116ec70d8f75559e481338c220f8
Frazer Smith <frazer.dev@icloud.com>
2025-06-25 19:26:45 +0100

    build(deps-dev): migrate to eslint flat config (#142)

--- src/index.test.js
+++ src/index.test.js
@@ -1,5 +1,6 @@
 "use strict";
 
+// eslint-disable-next-line n/no-unsupported-features/node-builtins -- Tests, not in distributed code
 const { after, before, describe, it } = require("node:test");
 const Fastify = require("fastify");
 const plugin = require(".");
