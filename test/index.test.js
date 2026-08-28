69dba8daf359709a5fd5235854a9d75dc8335876
Frazer Smith <frazer.dev@icloud.com>
2026-08-28 16:06:51 +0100

    perf(index): skip content negotiation that cannot return xml

--- test/index.test.js
+++ test/index.test.js
@@ -15,6 +15,8 @@ describe("JSON-To-XML plugin", () => {
 		"test-key": "test-value",
 		"$test-key": "test-value",
 	};
+	const cleanBody = { "test-key": "test-value" };
+	const jsonSequenceBody = `\u001E${JSON.stringify(cleanBody)}\n`;
 
 	before(async () => {
 		server = Fastify();
@@ -25,6 +27,20 @@ describe("JSON-To-XML plugin", () => {
 					.register(plugin)
 					.get("/no-replace", (_req, res) => {
 						res.send(resBody);
+					})
+					.get("/buffer", (_req, res) => {
+						res.type("application/json").send(
+							Buffer.from(JSON.stringify(cleanBody))
+						);
+					})
+					.get("/text", (_req, res) => {
+						res.type("text/plain").send("test-value");
+					})
+					.get("/uppercase-content-type", (_req, res) => {
+						res.type("Application/JSON").send(cleanBody);
+					})
+					.get("/json-seq", (_req, res) => {
+						res.type("application/json-seq").send(jsonSequenceBody);
 					});
 			})
 			.register(async (replaceInvalidCharContext) => {
@@ -70,6 +86,16 @@ describe("JSON-To-XML plugin", () => {
 					accept: "application/json, application/xml",
 				},
 			},
+			{
+				testName: "JSON body if accept header is absent",
+				headers: {},
+			},
+			{
+				testName: "JSON body if accept header is empty",
+				headers: {
+					accept: "",
+				},
+			},
 		];
 		const jsonTestsLength = jsonTests.length;
 
@@ -110,50 +136,118 @@ describe("JSON-To-XML plugin", () => {
 					accept: "application/xml, application/json",
 				},
 			},
+			{
+				testName:
+					"'APPLICATION/XML' as only value in accept HTTP request header",
+				headers: {
+					accept: "APPLICATION/XML",
+				},
+			},
+			{
+				testName:
+					"'application/json' rejected with 'q=0' and a wildcard in accept HTTP request header",
+				headers: {
+					accept: "application/json;q=0, */*",
+				},
+			},
+			{
+				testName: "default browser accept HTTP request header",
+				headers: {
+					accept: "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,*/*;q=0.8",
+				},
+			},
 		];
 		const xmlTestsLength = xmlTests.length;
 
 		for (let i = 0; i < xmlTestsLength; i += 1) {
 			const { testName, headers } = xmlTests[i];
 			// eslint-disable-next-line no-loop-func -- server never reassigned
-			describe(testName, () => {
-				it("Returns HTTP status code 500 due to invalid XML characters", async (/** @type {TestContext} */ t) => {
-					const response = await server.inject({
-						method: "GET",
-						url: "/no-replace",
-						headers,
-					});
-
-					t.plan(3);
-					t.assert.strictEqual(
-						response.body,
-						'<?xml version="1.0" encoding="UTF-8"?><response><statusCode>500</statusCode><error>Internal Server Error</error><message>in XML document > element "response": element name "$test-key" should not contain characters not allowed in XML names</message></response>'
-					);
-					t.assert.strictEqual(
-						response.headers["content-type"],
-						"application/xml; charset=utf-8"
-					);
-					t.assert.strictEqual(response.statusCode, 500);
+			it(`Returns XML responses for ${testName}`, async (/** @type {TestContext} */ t) => {
+				const noReplaceResponse = await server.inject({
+					method: "GET",
+					url: "/no-replace",
+					headers,
+				});
+				const replaceResponse = await server.inject({
+					method: "GET",
+					url: "/replace",
+					headers,
 				});
 
-				it("Returns XML body with invalid XML characters replaced", async (/** @type {TestContext} */ t) => {
-					const response = await server.inject({
-						method: "GET",
-						url: "/replace",
-						headers,
-					});
+				t.plan(6);
+				t.assert.strictEqual(
+					noReplaceResponse.body,
+					'<?xml version="1.0" encoding="UTF-8"?><response><statusCode>500</statusCode><error>Internal Server Error</error><message>in XML document > element "response": element name "$test-key" should not contain characters not allowed in XML names</message></response>'
+				);
+				t.assert.strictEqual(
+					noReplaceResponse.headers["content-type"],
+					"application/xml; charset=utf-8"
+				);
+				t.assert.strictEqual(noReplaceResponse.statusCode, 500);
+				t.assert.strictEqual(
+					replaceResponse.body,
+					'<?xml version="1.0" encoding="UTF-8"?><response><test-key>test-value</test-key><\uFFFDtest-key>test-value</\uFFFDtest-key></response>'
+				);
+				t.assert.strictEqual(
+					replaceResponse.headers["content-type"],
+					"application/xml; charset=utf-8"
+				);
+				t.assert.strictEqual(replaceResponse.statusCode, 200);
+			});
+		}
+	});
 
-					t.plan(3);
-					t.assert.strictEqual(
-						response.body,
-						'<?xml version="1.0" encoding="UTF-8"?><response><test-key>test-value</test-key><�test-key>test-value</�test-key></response>'
-					);
-					t.assert.strictEqual(
-						response.headers["content-type"],
-						"application/xml; charset=utf-8"
-					);
-					t.assert.strictEqual(response.statusCode, 200);
+	describe("Content-Type handling", () => {
+		const contentTypeTests = [
+			{
+				testName: "Returns unchanged body if payload is not a string",
+				url: "/buffer",
+				expectedBody: JSON.stringify(cleanBody),
+				expectedContentType: "application/json",
+			},
+			{
+				testName:
+					"Returns unchanged body if content-type is not 'application/json'",
+				url: "/text",
+				expectedBody: "test-value",
+				expectedContentType: "text/plain",
+			},
+			{
+				testName: "Returns an application/json-seq body unchanged",
+				url: "/json-seq",
+				expectedBody: jsonSequenceBody,
+				expectedContentType: "application/json-seq; charset=utf-8",
+			},
+			{
+				testName: "Returns XML body if content-type is not lowercase",
+				url: "/uppercase-content-type",
+				expectedBody:
+					'<?xml version="1.0" encoding="UTF-8"?><response><test-key>test-value</test-key></response>',
+				expectedContentType: "application/xml; charset=utf-8",
+			},
+		];
+		const contentTypeTestsLength = contentTypeTests.length;
+
+		for (let i = 0; i < contentTypeTestsLength; i += 1) {
+			const { testName, url, expectedBody, expectedContentType } =
+				contentTypeTests[i];
+			// eslint-disable-next-line no-loop-func -- server never reassigned
+			it(testName, async (/** @type {TestContext} */ t) => {
+				const response = await server.inject({
+					method: "GET",
+					url,
+					headers: {
+						accept: "application/xml",
+					},
 				});
+
+				t.plan(3);
+				t.assert.strictEqual(response.body, expectedBody);
+				t.assert.strictEqual(
+					response.headers["content-type"],
+					expectedContentType
+				);
+				t.assert.strictEqual(response.statusCode, 200);
 			});
 		}
 	});
